2185ccb0812fa05ca8e5285c0ec1b42efef64e06
Nikita <nikita.sadnani@trilio.io>
2021-09-21 05:56:35 +0000

    correcting nfsserver

--- tools/tvk-oneclick/tvk-oneclick.sh
+++ tools/tvk-oneclick/tvk-oneclick.sh
@@ -623,8 +623,10 @@ EOF
         if [[ -z "$nfs_options" ]]; then
           nfs_options='nfsvers=4'
         fi
-	set -x
 	echo "Creating target..."
+	echo "Nfs server= ${nfs_server}"
+	echo "Nfs option = ${nfs_options}"
+	echo "target_namespace = ${target_namespace}"
 	cat <<EOF | kubectl apply -f - 1>> >(logit) 2>> >(logit)
 apiVersion: triliovault.trilio.io/v1
 kind: Target
